aeb025e779f5964e7b8f37a5283c387870657b21
Erik Körberg <korbergerik@gmail.com>
2023-10-18 15:29:34 +0200

    Added cookie functionality to only show welcome page once

--- Manero/Controllers/WelcomeController.cs
+++ Manero/Controllers/WelcomeController.cs
@@ -6,6 +6,19 @@ namespace Manero.Controllers
     {
         public IActionResult Index()
         {
+            // Checks the cookies if it has a "hasSeenWelcome" cookie
+            if (!Request.Cookies.ContainsKey("hasSeenWelcome"))
+            {
+                // Set the "hasSeenWelcome" cookie to indicate that the user has seen the welcome page
+                var cookieOptions = new CookieOptions
+                {
+                    Expires = DateTime.UtcNow.AddYears(1), // Cookie lasts for 1 year
+                    Path = "/"  //What pages the cookie is available from / meaning all
+                };
+                // Add cookie "hasSeenWelcome"
+                Response.Cookies.Append("hasSeenWelcome", "true", cookieOptions);
+            }
+
             return View();
         }
     }
