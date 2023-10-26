eb3bf40eba582970cd29d45297363b9b38547a1a
Rickard <ricki90>
2023-10-26 20:54:58 +0200

    Init and made Controller and View

---
+++ Manero/Controllers/CheckoutShipping.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Manero.Controllers
+{
+    public class CheckoutShipping : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
