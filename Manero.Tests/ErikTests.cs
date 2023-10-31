f85dc76270e0aaf7c11699e166602519bcaac3a4
Erik Körberg <korbergerik@gmail.com>
2023-10-31 13:25:38 +0100

    welcomepage test added

--- Manero.Tests/ErikTests.cs
+++ Manero.Tests/ErikTests.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Moq;
 using System.Linq.Expressions;
 using System.Net;
+using System.Net.Http;
 
 namespace Manero.Tests
 {
@@ -76,6 +77,34 @@ namespace Manero.Tests
         public void Dispose()
         {
             _context.Dispose();
+        }
+
+
+        [Fact]
+        public void CookieTest()
+        {
+            // Arrange
+            var controller = new WelcomeController();
+            var httpContext = new DefaultHttpContext();
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+            var expectedCookieName = "hasSeenWelcome";
+            var expectedCookieValue = "true"; // Ensure the expected value is a string as cookies store string-based values
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+
+            var setCookieHeader = httpContext.Response.Headers["Set-Cookie"];
+
+            // Convert the set-cookie header to a string and check if it contains the expected cookie name and value
+            var setCookieString = Assert.Single(setCookieHeader);
+            Assert.Contains(expectedCookieName, setCookieString);
+            Assert.Contains(expectedCookieValue, setCookieString);
         }
     }
 }
--- Manero/Controllers/WelcomeController.cs
+++ Manero/Controllers/WelcomeController.cs
@@ -1,20 +1,24 @@
-﻿using Manero.Helpers.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Manero.Controllers
 {
     public class WelcomeController : Controller
     {
-        private readonly ICookieService _cookieService;
-
-        public WelcomeController(ICookieService cookieService)
-        {
-            _cookieService = cookieService;
-        }
-
         public IActionResult Index()
         {
-            _cookieService.SetWelcomeCookie(HttpContext);
+            // Checks the cookies if it has a "hasSeenWelcome" cookie
+            if (!Request.Cookies.ContainsKey("hasSeenWelcome"))
+            {
+                // Set the "hasSeenWelcome" cookie to indicate that the user has seen the welcome page
+                var cookieOptions = new CookieOptions
+                {
+                    Expires = DateTime.UtcNow.AddYears(1), // Cookie lasts for 1 year
+                    Path = "/"  //What pages the cookie is available from / meaning all
+                };
+                // Add cookie "hasSeenWelcome"
+                Response.Cookies.Append("hasSeenWelcome", "true", cookieOptions);
+            }
+
             return View();
         }
     }
