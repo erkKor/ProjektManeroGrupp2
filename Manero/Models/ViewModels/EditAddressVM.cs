a5475f442b38a0586f9d2c53abc7afa563d2ace8
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2023-10-31 10:48:58 +0100

    Fixed saving addresses.
    - Fixed addresses not saved to server.
    - Added remove address button.
    - Added no addresses message.
    - Removed hardcoded test addresses.
    - Added AddressService.

--- Manero/Models/ViewModels/EditAddressVM.cs
+++ Manero/Models/ViewModels/EditAddressVM.cs
@@ -1,14 +1,13 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using Manero.Models.Entities;
 
 namespace Manero.Models.ViewModels
 {
     public class EditAddressVM
     {
 
-        public bool IsCreatingNew { get; set; }
-
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [Required, DisplayName("Name")]
         public string? AdressName { get; set; }
@@ -21,6 +20,24 @@ namespace Manero.Models.ViewModels
 
         [Required, DisplayName("City")]
         public string City { get; set; } = null!;
+
+        public static implicit operator AdressEntity(EditAddressVM view)
+        {
+
+            var entity = new AdressEntity
+            {
+                City = view.City,
+                PostalCode = view.PostalCode,
+                AdressName = view.AdressName,
+                StreetName = view.StreetName
+            };
+
+            if (view.Id.HasValue)
+                entity.Id = view.Id.Value;
+
+            return entity;
+
+        }
 
     }
 }
