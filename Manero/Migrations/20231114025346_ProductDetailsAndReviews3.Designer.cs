e3d43b3e487fb54b5086dad2716fffe29a4a4f55
Rainerson <raha1618@student.ju.se>
2023-11-14 04:01:15 +0100

    Om det inte finns någon rating, skriv ut tomma stjärnor

--- Manero/Migrations/20231114025346_ProductDetailsAndReviews3.Designer.cs
+++ Manero/Migrations/20231114025346_ProductDetailsAndReviews3.Designer.cs
@@ -4,6 +4,7 @@ using Manero.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Manero.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231114025346_ProductDetailsAndReviews3")]
+    partial class ProductDetailsAndReviews3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
