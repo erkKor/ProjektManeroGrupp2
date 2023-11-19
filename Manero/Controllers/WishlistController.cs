7e46e7637b95edecb538122ca9a8f9af7fb6893e
Jonathan Mellergård John <jonathan.mellergard@gmail.com>
2023-11-19 17:40:22 +0100

    -Added ability to to view added items in the wishlist

--- Manero/Controllers/WishlistController.cs
+++ Manero/Controllers/WishlistController.cs
@@ -18,6 +18,49 @@ namespace Manero.Controllers
             _dbContext = dbContext;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            // Ensure the user is authenticated
+            if (user == null)
+            {
+                return RedirectToAction("Index", "SignIn");
+            }
+
+            // Retrieve the user's wishlist and associated items
+            var wishlist = await _dbContext.WishLists
+                .Include(w => w.WishListItems)
+                .ThenInclude(item => item.Product) // Assuming Product is a navigation property in WishListItemEntity
+                .FirstOrDefaultAsync(w => w.UserId == user.Id);
+
+            if (wishlist == null)
+            {
+                // Wishlist not found, you can handle this case based on your requirements
+                return NotFound();
+            }
+
+            // convert WishList to List<ProductEntity>
+
+            var items = wishlist.WishListItems;
+            var products = new List<ProductEntity>();
+            foreach (var item in items)
+            {
+                var product = new ProductEntity()
+                {
+                    Id = item.ProductId,
+                    Name = item.Product.Name,
+                    Description = item.Product.Description,
+                    Rating = item.Product.Rating,
+                    Price = item.Product.Price,
+                };
+                products.Add(product);
+            }
+
+            // You can pass the wishlist to the view for rendering
+            return View(products);
+        }
+
         // Action to add a product to the wishlist
         public async Task<IActionResult> AddToWishlist(int productId)
         {
@@ -37,32 +80,28 @@ namespace Manero.Controllers
                 .Where(w => w.UserId == user.Id)
                 .FirstOrDefaultAsync();
 
-            var item = new WishListItemEntity
+            // retrieve WishListItems
+
+            List<WishListItemEntity> wishListItems = await _dbContext.WishListItems.Where(i => i.WishListId == wishList.Id).ToListAsync();
+
+            // check if product already exists in wishList
+            if(!wishListItems.Any(i => i.ProductId == productId)) 
             {
-                ProductId = productId,
-                WishListId = wishList.Id
-            };
+                var item = new WishListItemEntity
+                {
+                    ProductId = productId,
+                    WishListId = wishList.Id
+                };
 
-            user.WishList.WishListItems.Add(item);
+                user.WishList.WishListItems.Add(item);
 
-            await _dbContext.SaveChangesAsync();
+                await _dbContext.SaveChangesAsync();
+            }
+
+            
 
             return NoContent();
 
-        }
-
-        // Action to view the wishlist
-        public async Task<IActionResult> ViewWishlist()
-        {
-            var user = await _userManager.GetUserAsync(User);
-
-            // Ensure the user is authenticated
-            if (user == null)
-            {
-                return RedirectToAction("Login", "Account");
-            }
-
-            return View(user.WishList.WishListItems);
-        }
+        }      
     }
 }
